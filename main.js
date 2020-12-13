70d1f282c014549fa5709c69f746c789e8c46327
Yovie <192410101011@students.unej.ac.id>
2020-12-13 18:36:23 +0700

    tugas

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("selamat datang!")
+function TerimaInput(){
+    var Nama = document.querySelector("#Nama").value;
+    var NIM = document.querySelector("#NIM").value;
+    console.log('Nama : ',Nama)
+    console.log('NIM : ',NIM)
+}
